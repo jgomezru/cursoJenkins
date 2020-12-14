05686cf3798ef3190edc1352aa7a8358434ddabc
jgomezru <jorge.gomez-ruiz@capgemini.com>
2020-12-14 18:38:04 +0000

    Subiendo archivos de forma automatica

--- miproyecto/src/main/java/es/curso/App.java
+++ miproyecto/src/main/java/es/curso/App.java
@@ -16,4 +16,13 @@ public class App
         return numero *2;
         
     }
+    
+    
+    public static int mayor (int numero1 , int numero2)
+    {
+        
+        
+        return numero1>numero2 ? numero1 : numero2;
+        
+    }
 }
--- miproyecto/src/test/java/es/curso/AppTest.java
+++ miproyecto/src/test/java/es/curso/AppTest.java
@@ -17,4 +17,59 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    
+   @Test
+    public void pruebaDoblar()
+    {
+        int resultado=App.doblar(5);
+        assertEquals(resultado,10);
+    }
+    
+   @Test
+    public void pruebaDoblarNegativo()
+    {
+        int resultado=App.doblar(-5);
+        assertEquals(resultado,-10);
+    }    
+    
+   @Test
+    public void pruebaDoblarCero()
+    {
+        int resultado=App.doblar(0));
+        assertEquals(resultado,0);
+    }       
+    
+   @Test
+    public void pruebaDoblarCero()
+    {
+        int resultado=App.doblar(0));
+        assertEquals(resultado,0);
+    }       
+    
+    
+  @Test
+    public void pruebaMayor1()
+    {
+        int resultado=App.mayor(2,0));
+        assertEquals(resultado,2);
+    }       
+
+  @Test
+    public void pruebaMayor2()
+    {
+        int resultado=App.mayor(0,5));
+        assertEquals(resultado,5);
+    }      
+        
+  @Test
+    public void pruebaMayorNegativo()
+    {
+        int resultado=App.mayor(0,-5));
+        assertEquals(resultado,0);
+    }          
+        
+        
+        
+    
 }
