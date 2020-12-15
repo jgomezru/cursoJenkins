1d2a35f5b4b10c62290bc644f0930d3314b9401d
jgomezru <jorge.gomez-ruiz@capgemini.com>
2020-12-15 17:12:55 +0000

    Subiendo archivos de forma automatica dia 15

--- miproyecto/src/main/java/es/curso/App.java
+++ miproyecto/src/main/java/es/curso/App.java
@@ -1,17 +1,24 @@
-
-
 package es.curso;
+import java.io.IOException; 
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+import java.util.logging.*; 
 
 /**
  * Hello world!
  *
  */
 public class App 
+
 {
+     private final static Logger LOGGER =  
+                Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+                
     public static void main( String[] args )
     {
         System.out.println( "Hola Mundo!" );
         /*logger.log("My Message");*/
+         LOGGER.log(Level.INFO, "Hola Mundo pero en el log");
     }
     
     public static int doblar (int numero)
