d2f5d59cc75c766044c91ba6db42c13111f0b32a
jgomezru <jorge.gomez-ruiz@capgemini.com>
2020-12-16 15:17:34 +0000

    Subiendo archivos de forma automatica dia 16

--- ejemplo-web/compilamaven.sh
+++ ejemplo-web/compilamaven.sh
@@ -3,10 +3,10 @@ mvn compile
 mvn process-classes
 mvn test-compile
 mvn process-test-sources
-generate-test-resources
-process-test-resources
+mvn generate-test-resources
+mvn process-test-resources
 mvn test
-prepare-package
+mvn prepare-package
 mvn package
 mvn verify
 mvn install
--- subegit.sh
+++ subegit.sh
@@ -17,7 +17,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-  message="Subiendo archivos de forma automatica dia 15"
+  message="Subiendo archivos de forma automatica dia 16"
 fi
 
 # Haciendo los git add/commit/push por steps
